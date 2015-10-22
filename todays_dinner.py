b1b456d6b1f05d27112d92b7a007c51d459165b6
Vegar Molvig <vegar.molvig@gmail.com>
2015-10-22 15:44:42 +0200

    Major release
    I can't even hold all these production ready lines of code, so pretty.
    Amazing
    Clean-code (not)
    
    Henning approves, much. Love. Jizz.
    
    API Structure is completely different, nothing is backwards compatible.
    Too bad for you :̈́'(
    
    In the future, the API will still not be stable, and changes may apply. Much.
    
    /V

--- todays_dinner.py
+++ todays_dinner.py
@@ -1,39 +1,63 @@
 #!/usr/bin/env python3
 from flask import Flask, jsonify, request
+from werkzeug.contrib.cache import SimpleCache
 from bs4 import BeautifulSoup
-import requests
+import requests, os, time
 
 todays_dinner_url = 'https://beta.sio.no/mat-og-drikke/_window/mat+og+drikke+-+dagens+middag?s={}'
+
+# Static things
 cafeteria_ids = {
         'informatikk': 284,
         'frederikke': 122,
         'kutt': 310
         }
+api_url = 'http://api.desperate.solutions/dagens/'
+cache = SimpleCache()
 
 app = Flask(__name__)
 
-def todays_dinner_to_dict(html, own_url):
-    soup = BeautifulSoup(html, 'html.parser')
+def get_dishes(cafeteria):
+    dishes = cache.get(cafeteria)
+    if dishes is None:
+        print("{} is not cached, getting from web.".format(cafeteria))
+        dishes = get_todays_dinner_from_web(cafeteria)
+        cache.set(cafeteria, dishes, timeout=6*60*60)
+
+    return dishes
+
+def make_urls():
+    return {cafeteria: '{}/{}'.format(api_url[:api_url.rfind('/')], cafeteria) for cafeteria in cafeteria_ids.keys()}
+
+def get_todays_dinner_from_web(cafeteria):
+    todays_dinner_html = requests.get(todays_dinner_url.format(cafeteria_ids[cafeteria])).text
+    soup = BeautifulSoup(todays_dinner_html, 'html.parser')
     headers = soup.find_all('h3')
     paragraphs = soup.find_all('p')
+    dishes = []
 
     if len(headers) != len(paragraphs):
-        return {error: "Number of headers does not correspond to the number of paragraphs."}
-    
-    todays_dinner = {"dishes": [], "cafeterias": ['{}/{}'.format(own_url[:own_url.rfind('/')], cafeteria) for cafeteria in cafeteria_ids.keys()]}
-    for h, p in zip(headers, paragraphs):
-        todays_dinner["dishes"] += [{"category": h.contents[0].lower(), "dish": [span.contents[0] for span in p.find_all('span')]}]
+        return jsonify({
+            error: "Invalid response from beta.sio.no",
+            more_info: "Number of headers does not correspond to the number of paragraphs"
+            })
 
-    return todays_dinner
+    for h, p in zip(headers, paragraphs):
+        dishes.append({
+            "category": h.contents[0].lower(),
+            "dishes": [span.contents[0] for span in p.find_all('span')]})
+
+    return dishes
 
 @app.route('/todays_dinner/', methods=['GET'])
 @app.route('/todays_dinner/<cafeteria>', methods=['GET'])
-def get_todays_dinner(cafeteria='informatikk'):
-    if not cafeteria in cafeteria_ids.keys():
-        cafeteria = 'informatikk'
+def get_todays_dinner(cafeteria=None):
+    cafeterias = {}
 
-    todays_dinner_html = requests.get(todays_dinner_url.format(cafeteria_ids[cafeteria])).text
-    return jsonify(todays_dinner_to_dict(todays_dinner_html, request.base_url))
+    if cafeteria in cafeteria_ids.keys():
+        return jsonify({'cafeteria': get_dishes(cafeteria)})
+    else:
+        return jsonify(make_urls())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
