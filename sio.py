44732837a66aba8378b8624dcdfd68bd5ed5f083
Vegar Molvig <vegar.molvig@gmail.com>
2015-11-08 14:19:29 +0100

    Rewrite
    Moved the code that fetched todays_dinner from sio to another file (sio.py),
    and kept the web/api/flask relevant stuff in todays_dinner.py.

---
+++ sio.py
@@ -0,0 +1,64 @@
+from bs4 import BeautifulSoup
+import requests, os, time, json, memcache
+
+TODAYS_DINNER_URL = 'https://beta.sio.no/mat-og-drikke/_window/mat+og+drikke+-+dagens+middag?s={}'
+
+class Menu:
+    def __init__(self):
+        self.dishes = {}
+
+
+class TodaysDinner:
+    def __init__(self):
+        self.cache = memcache.Client(['127.0.0.1:11211'])
+
+        with open('cafeterias.json') as f:
+            self.cafeteria_ids = json.load(f)
+
+    def flush_cache(self, cafeteria):
+        self.cache.delete(cafeteria)
+
+    def get_menu(self, cafeteria):
+        menu = self.cache.get(cafeteria)
+        if menu is None:
+            print("{} is not cached, getting from web.".format(cafeteria))
+            menu = self.get_todays_dinner_from_web(cafeteria)
+
+            # If the menu is not published yet, dont cache it. But we want to
+            # return whatever it is to the users of the API.
+            # It can either be en empty response,
+            if len(menu) == 0:
+                return menu
+            # Or the text will say something like this.
+            for category in menu:
+                for dish in category['dishes']:
+                    if "dagens meny legges ut" in dish.lower():
+                        return menu
+
+            self.cache.add(cafeteria, menu, time=1*60*60)
+
+        return menu
+
+    def get_todays_dinner_from_web(self, cafeteria):
+        todays_dinner_html = requests.get(TODAYS_DINNER_URL.format(self.cafeteria_ids[cafeteria])).text
+
+        soup = BeautifulSoup(todays_dinner_html, 'html.parser')
+
+        headers = soup.find_all('h3')
+        paragraphs = soup.find_all('p')
+
+        menu = []
+
+        if len(headers) != len(paragraphs):
+            # Number of headers does not correspond to the number of paragraphs
+            return jsonify({error: "Invalid response from beta.sio.no"})
+
+        for h, p in zip(headers, paragraphs):
+            dishes = [span.contents[0].lstrip(' -') for span in p.find_all('span')]
+
+            menu.append({
+                "category": h.contents[0].lower(),
+                "cacheTimestamp": time.time(),
+                "dishes": dishes})
+
+        return menu
--- todays_dinner.py
+++ todays_dinner.py
@@ -1,66 +1,21 @@
 #!/usr/bin/env python3
 from flask import Flask, jsonify, request, g
-from werkzeug.contrib.cache import SimpleCache
-from bs4 import BeautifulSoup
-import requests, os, time, json
+from werkzeug.contrib.cache import MemcachedCache
+from sio import TodaysDinner
 
-# Static things
-todays_dinner_url = 'https://beta.sio.no/mat-og-drikke/_window/mat+og+drikke+-+dagens+middag?s={}'
-cafeteria_ids = {}
-api_url = 'http://api.desperate.solutions/dagens/'
-api_urls = {}
-cache = SimpleCache()
+api_base_url = 'http://api.desperate.solutions/dagens/'
+todays_dinner = TodaysDinner()
+
+api_base_urls = {cafeteria:
+'{}/{}'.format(api_base_url[:api_base_url.rfind('/')], cafeteria) for cafeteria in todays_dinner.cafeteria_ids.keys()}
 
 app = Flask(__name__)
-
-def get_menu(cafeteria):
-    menu = cache.get(cafeteria)
-    if menu is None:
-        print("{} is not cached, getting from web.".format(cafeteria))
-        menu = get_todays_dinner_from_web(cafeteria)
-        if len(menu) == 0:
-            return menu
-
-        for category in menu:
-            for dish in category['dishes']:
-                if "dagens meny legges ut" in dish.lower():
-                    return menu
-
-
-        cache.set(cafeteria, menu, timeout=6*60*60)
-
-    return menu
-
-def make_urls():
-    return {cafeteria: '{}/{}'.format(api_url[:api_url.rfind('/')], cafeteria) for cafeteria in cafeteria_ids.keys()}
-
-def get_todays_dinner_from_web(cafeteria):
-    todays_dinner_html = requests.get(todays_dinner_url.format(cafeteria_ids[cafeteria])).text
-    soup = BeautifulSoup(todays_dinner_html, 'html.parser')
-    headers = soup.find_all('h3')
-    paragraphs = soup.find_all('p')
-    menu = []
-
-    if len(headers) != len(paragraphs):
-        # Number of headers does not correspond to the number of paragraphs
-        return jsonify({error: "Invalid response from beta.sio.no"})
-
-    for h, p in zip(headers, paragraphs):
-        dishes = [span.contents[0].strip(' -') for span in p.find_all('span')]
-
-        menu.append({
-            "category": h.contents[0].lower(),
-            "cacheTimestamp": time.time(),
-            "dishes": dishes})
-
-    return menu
-
 @app.route('/todays_dinner/<cafeteria>/flush', methods=['GET'])
 def flush_cache(cafeteria=None):
     if not cafeteria:
         return jsonify({error: "Invalid or no cafeteria specified"})
     else:
-        cache.delete(cafeteria)
+        todays_dinner.flush_cache(cafeteria)
         return get_todays_dinner(cafeteria)
 
 @app.route('/todays_dinner/', methods=['GET'])
@@ -68,13 +23,10 @@ def flush_cache(cafeteria=None):
 def get_todays_dinner(cafeteria=None):
     cafeterias = {}
 
-    if cafeteria in cafeteria_ids.keys():
-        return jsonify({'cafeteria': get_menu(cafeteria)})
+    if cafeteria in todays_dinner.cafeteria_ids.keys():
+        return jsonify({'cafeteria': todays_dinner.get_menu(cafeteria)})
     else:
-        return jsonify(api_urls)
+        return jsonify(api_base_urls)
 
 if __name__ == '__main__':
-    with open('cafeterias.json') as f:
-        cafeteria_ids = json.load(f)
-    api_urls = make_urls()
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5001, debug=True)
